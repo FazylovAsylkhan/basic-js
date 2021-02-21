b1371ca3056cb468824f22e046cf512b9c317270
Fazylov Asylkhan <fazylov.asylkhan@gmail.com>
2021-02-21 14:13:55 +0500

    feat: do task extended-repeater

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 'use strict'
 
-class DepthCalculator {
-  calculateDepth(arr) {
-    let newArr = arr.filter(item => Array.isArray(item));
-
-    if (newArr.length == 0) {
-      return 1
-    } else {
-      newArr = newArr.map(item => this.calculateDepth(item)).sort((a,b) => a - b)
-      console.log(newArr);
-      return 1 + newArr[newArr.length - 1];
+function repeater(str, options) {
+  let countOfProp = Object.keys(options).length;
+  console.log(countOfProp);
+  if (countOfProp == 1) {
+    typeof options.repeatTimes == 'undefined' ? options.repeatTimes = 1 : options.repeatTimes;
+    return recursiveRepeater(str, options.repeatTimes).split(',').join('+')
+  } else if (countOfProp == 2) {
+    return recursiveRepeater(str, options.repeatTimes).split(',').join(`${options.separator}`);
+  } else if (countOfProp == 3) {
+    typeof options.repeatTimes == 'undefined' ? options.repeatTimes = 1 : options.repeatTimes;
+    typeof options.additionRepeatTimes == 'undefined' ? options.additionRepeatTimes = 1 : options.additionRepeatTimes;
+    let addition = recursiveRepeater(options.addition, options.additionRepeatTimes).split(',').join('+')
+    return recursiveRepeater(str, options.repeatTimes).split(',').join(`${addition}`);
+  } else {
+    typeof options.repeatTimes == 'undefined' ? options.repeatTimes = 1 : options.repeatTimes;
+    typeof options.additionRepeatTimes == 'undefined' ? options.additionRepeatTimes = 1 : options.additionRepeatTimes;
+    let addition = recursiveRepeater(options.addition, options.additionRepeatTimes).split(',').join(`${options.additionSeparator}`);
+    return recursiveRepeater(str + addition, options.repeatTimes).split(',').join(`${options.separator}`);
+  } 
+  
+  function recursiveRepeater (str, number) {
+    if (number == 1) {
+      return str;
     }
+    
+    return str + ',' + recursiveRepeater(str, number - 1);
   }
 };
 
 
-const instance = new DepthCalculator();
-instance.calculateDepth([[5, 4], [2],2,[4]]);
+// let arr = Object.keys({ repeatTimes: 3, separator: '**', addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' });
+let test = repeater('STRING', { repeatTimes: 3, separator: '**', addition: 'PLUS'});
+// console.log(arr.length);
+console.log(test);
+
+console.log(Array.isArray([1,2,3,'4']));
+
+Array.isArray([1,2,3,'4'])
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let countOfProp = Object.keys(options).length;
+  let repeatTimes =  options.repeatTimes,
+      separator = options.separator,
+      addition = options.addition,
+      additionRepeatTimes = options.additionRepeatTimes,
+      additionSeparator = options.additionSeparator;
+
+  if (countOfProp == 1) {
+    return createStr(str, repeatTimes);
+  } else if (countOfProp == 2) {
+    return createStr(str, repeatTimes, separator);
+  } else if (countOfProp == 3) {
+    addition = createStr(addition, additionRepeatTimes);
+    return createStr(str + addition, repeatTimes, separator);
+  } else {
+    addition = createStr(addition, additionRepeatTimes, additionSeparator);
+    return createStr(str + addition, repeatTimes, separator);
+  } 
+
+  function createStr(string, repeatTimes = 1, separator = '+') {
+    return recursiveRepeater(string, repeatTimes).split('_').join(`${separator}`);
+  }
+  function recursiveRepeater(string, number) {
+    if (number == 1) {
+      return string;
+    }    
+    return string + '_' + recursiveRepeater(string, number - 1);
+  }
 };
   
